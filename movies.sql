65b1da8188b759b635339a0b6ee9aafb1fc9f4a1
andrewbowen19 <atbowen@gmail.com>
2022-09-07 22:20:18 -0400

    Add movies ranking file and HW 2

---
+++ movies.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS movie_rantings;
+
+CREATE TABLE movie_rankings
+	(rating_timestamp timestamp,
+     critic varchar(50),
+     shrek int,
+     godfather int,
+     remember_the_titans int,
+     titanic int,
+     anchorman int
+     
+    )
+;
